b364ff8082fdcc28dfdf44c8a5304a9024ab248a
Joseph <josembi@gmail.com>
2018-02-22 16:07:09 +0000

    Add Devise

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -58,4 +58,7 @@ Rails.application.configure do
   # Mail Catcher
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = { :address => "localhost", :port => 1025 } 
+
+  # Devise Mail Management
+  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
 end
