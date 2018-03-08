806f57b0340886bc49345bf8ee9bda34d2d174a8
Joseph <josembi@gmail.com>
2018-03-08 17:21:48 +0000

    Create Migrations for Teams

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180301201608) do
+ActiveRecord::Schema.define(version: 20180308171955) do
+
+  create_table "teams", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.integer "user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
