7d28fa64b038668021593e891b338633909aa8da
Joseph <josembi@gmail.com>
2018-03-10 21:41:46 +0000

    Add Edit Page for Teams

--- app/controllers/teams_controller.rb
+++ app/controllers/teams_controller.rb
@@ -31,7 +31,7 @@ class TeamsController < ApplicationController
 
     respond_to do |format|
       if @team.save
-        format.html { redirect_to @team, notice: 'Team was successfully created.' }
+        format.html { redirect_to root_path, notice: 'Team was successfully created.' }
         format.json { render :show, status: :created, location: @team }
       else
         format.html { render :new }
